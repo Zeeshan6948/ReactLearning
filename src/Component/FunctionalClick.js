f857fde16546d159331d7ee56463dd60481ae1ed
Zeeshan6948 <zeeshan6948@gmail.com>
2022-07-09 01:09:53 +0200

    Little Changes

--- src/Component/FunctionalClick.js
+++ src/Component/FunctionalClick.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FunctionalClick() {
     function clickHandler(){
-        console.log('button Clicked');
+        console.log('button Clicked my');
     }
   return (
     <div><button onClick={clickHandler}>click</button></div>
